5b43b22088e74a7bfdabc9fa78c86c40a5a35b81
Daniel <gamerpcx@outlook.com>
2018-05-28 11:10:23 -0700

    test
    test better

--- Jumpingtruck/Source/Jumpingtruck/Badguy.h
+++ Jumpingtruck/Source/Jumpingtruck/Badguy.h
@@ -24,6 +24,6 @@ public:
 	virtual void Tick(float DeltaSeconds) override;
 
 	//Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* InputCompont)
-
-}
+	virtual void SetupPlayerInputComponent(class UInputComponent* InputCompont) override;
+	
+};
